9bb07c88a85645d4da460053b21f9df4b2db32d9
antoniogiordano <antonio.giordano@getapper.com>
2021-01-20 15:39:12 +0100

    MUI comp command added Init command fixes

--- generators/init/templates/src/AppWrapper.tsx
+++ generators/init/templates/src/AppWrapper.tsx
@@ -4,20 +4,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor, history } from "redux-store";
 import { ConnectedRouter } from "connected-react-router";
 
-/**
- * @TODO: Import this wrapper in your index.tsx file like this:
- *
- * ReactDOM.render(
- *   <React.StrictMode>
- *     <AppWrapper>
- *       <App />
- *     </AppWrapper>
- *   </React.StrictMode>,
- *   document.getElementById('root')
- * );
- *
- * Where Index is your main React UI component
- */
 const AppWrapper: React.FC<{children: ReactNode}> = (props) => {
   return (
     <Provider store={store}>
